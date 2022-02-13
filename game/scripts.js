37bf4677e30884937b9ade0bf17eaba4b668615a
Aleksandr <soft@vhlv.ru>
2022-02-13 23:51:28 +0300

    feat: add randome zero

--- game/scripts.js
+++ game/scripts.js
@@ -1,19 +1,44 @@
 const gameField = document.querySelector('.game__field');
 
+const win = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 let step = 0;
+
+const countStep = () => ++step;
+
+const choiceCell = () => {
+  const emptyCells = document.querySelectorAll('.empty');
+  const randomCell = Math.floor(Math.random() * emptyCells.length);
+  emptyCells[randomCell].classList.add('zero');
+  emptyCells[randomCell].classList.remove('empty');
+  countStep();
+};
 
 const addCross = (e) => {
   if (e.target.classList.contains('game__field-cell')) {
-    step += 1;
     e.target.classList.add('cross');
+    e.target.classList.remove('empty');
   }
+  countStep();
+  choiceCell();
 };
 
-const addZero = (e) => {
-  if (e.target.classList.contains('game__field-cell')) {
-    e.target.classList.add('zero');
-    step += 1;
-  }
-};
+// const addZero = (e) => {
+//   if (e.target.classList.contains('game__field-cell')) {
+//     e.target.classList.add('zero');
+//     e.target.classList.add('disabled');
+//
+//   }
+// countStep();
+// };
 
-gameField.addEventListener('click', (e) => (step % 2 === 0 ? addCross(e) : addZero(e)));
+gameField.addEventListener('click', addCross);
