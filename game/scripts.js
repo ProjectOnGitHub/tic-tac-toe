4d81d1d0b2f548bf3a7ab19c5c8ad353f8647525
Aleksandr <soft@vhlv.ru>
2022-02-13 21:10:54 +0300

    feat: add zero

--- game/scripts.js
+++ game/scripts.js
@@ -1,10 +1,19 @@
 const gameField = document.querySelector('.game__field');
 
+let step = 0;
+
 const addCross = (e) => {
   if (e.target.classList.contains('game__field-cell')) {
+    step += 1;
     e.target.classList.add('cross');
-    console.log('тык');
   }
 };
 
-gameField.addEventListener('click', addCross);
+const addZero = (e) => {
+  if (e.target.classList.contains('game__field-cell')) {
+    e.target.classList.add('zero');
+    step += 1;
+  }
+};
+
+gameField.addEventListener('click', (e) => (step % 2 === 0 ? addCross(e) : addZero(e)));
