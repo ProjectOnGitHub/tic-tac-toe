5f4c5d95d37e20a2bdbe5a4b31ee4587fea5af8c
Aleksandr <soft@vhlv.ru>
2022-02-14 17:12:51 +0300

    feat: add save result to array

--- game/scripts.js
+++ game/scripts.js
@@ -1,5 +1,4 @@
 const gameField = document.querySelector('.game__field');
-
 const win = [
   [0, 1, 2],
   [3, 4, 5],
@@ -13,13 +12,23 @@ const win = [
 
 let step = 0;
 
+const crossResults = [];
+const zeroResults = [];
+
 const countStep = () => ++step;
+
+const addResult = (arr, cell) => {
+  const cells = document.querySelectorAll('.game__field-cell');
+  const index = Array.from(cells).indexOf(cell);
+  arr.push(index);
+};
 
 const choiceCell = () => {
   const emptyCells = document.querySelectorAll('.empty');
   const randomCell = Math.floor(Math.random() * emptyCells.length);
   emptyCells[randomCell].classList.add('zero');
   emptyCells[randomCell].classList.remove('empty');
+  addResult(zeroResults, emptyCells[randomCell]);
   countStep();
 };
 
@@ -27,6 +36,7 @@ const addCross = (e) => {
   if ((e.target.classList.contains('game__field-cell')) && (step % 2 === 0)) {
     e.target.classList.add('cross');
     e.target.classList.remove('empty');
+    addResult(crossResults, e.target);
     countStep();
     choiceCell();
   }
